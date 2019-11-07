33f5c4df9e33fcbf1a18dd971471a3c64cfddb3e
Gijsbertk <55399498+Gijsbertk@users.noreply.github.com>
2019-11-07 19:45:40 +0100

    Update 6.7.py

--- 6.7.py
+++ 6.7.py
@@ -1,22 +1,23 @@
-def main():
-    aantal_woorden = vraag()
-    verwerk(aantal_woorden)
+def main(): 
+    aantal_woorden = vraag() #roept fucntie aan en returned bestand
+    verwerk(aantal_woorden) #verwerkt return en roept bestand aan
 
 
-def verwerk(aantal_woorden):
-    bestand = open('woorden.txt', 'a')
-    for x in range(aantal_woorden):
-        woord = input('voer je woord in: ')
-        bestand.write(str(woord) + '\n')
-    bestand.close()
+def verwerk(aantal_woorden): #functie en verwerkt return
+    bestand = open('woorden.txt', 'a') #open/creert nieuw bestand
+    for x in range(aantal_woorden): # itereety met varaiable
+        woord = input('voer je woord in: ') #itereert de var 
+        bestand.write(str(woord) + '\n') #itereert de var
+    bestand.close() # sluit bestand 
         
 
 
-def vraag():
-    aantal_woorden = int(input('Hoeveel woorden wil je invoeren?: '))
-    return aantal_woorden
+def vraag(): #functie
+    aantal_woorden = int(input('Hoeveel woorden wil je invoeren?: ')) #vraagt input voor aantal woornden
+    return aantal_woorden #returned variabele
 
 
 
 
 main()
+#auteur gijsbert keja
