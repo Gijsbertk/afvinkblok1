4565019afb3d53fa379786706afb5f3bf5571681
Gijsbertk <55399498+Gijsbertk@users.noreply.github.com>
2019-11-07 20:16:34 +0100

    Update 4.12.py

--- 4.12.py
+++ 4.12.py
@@ -3,4 +3,4 @@ getal = int(input('Waarvan wilt u de factoril weten?: ')) #vraagt om input
 for num in range(1, getal + 1): #itereert het aantal keer wat de input is gegeven
      fac = fac * num #de fac wordt uitgerekend dmv de aantal iteraties 
 print (fac) # print de fac
-    
+ #auteur gijsbert keja   
