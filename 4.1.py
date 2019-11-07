476196a57f137175a37922a6b22e7dc33c29c72b
Gijsbertk <55399498+Gijsbertk@users.noreply.github.com>
2019-11-07 20:16:25 +0100

    Update 4.1.py

--- 4.1.py
+++ 4.1.py
@@ -4,3 +4,4 @@ for dag in range(5): # itereert 5 keer
     bugs = int(input('hoeveel insecten heeft u vandaag gevangen?')) # deze statement wordt 5 keer geïtereerd
     total = bugs + total # de bugs worden per input bij elkaar opgeteld
 print('u heeft deze week',total,'insecten gevangen') # print het aantal bugs
+#auteur gijsbert keja
