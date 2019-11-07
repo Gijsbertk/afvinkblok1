417ead76b9779ae03785eb6f9304e96674ff8678
Gijsbertk <55399498+Gijsbertk@users.noreply.github.com>
2019-11-07 19:41:57 +0100

    Update 6.5.py

--- 6.5.py
+++ 6.5.py
@@ -1,6 +1,7 @@
-totaal = ""
-cijfer = input(open("bestandnaam")
-for x in getallen:
-    totaal += int (x)
-totaal = totaal / len(getallen)
-print (total)
+totaal = "" #variabele met spatie
+cijfer = input(open("bestandnaam") #opent bestand na input van de bestandsnaam
+for x in cijfer: #itereert bestand
+    totaal += int (x) #voegt iteraties toe
+totaal = totaal / len(getallen) #berekend totaal
+print (total) #print variabele
+#auteur gijsbert keja
