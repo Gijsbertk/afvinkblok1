258e39a7a6e9e2f96b46c78954133c2c177b2d6c
Gijsbertk <55399498+Gijsbertk@users.noreply.github.com>
2019-11-07 20:16:16 +0100

    Update 3.8.py

--- 3.8.py
+++ 3.8.py
@@ -6,3 +6,4 @@ elif getal in (1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36): #als getal is 
     print('Het getal',getal,'is rood')
 elif getal in (2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35): #als getal is een van de volgende getallen print roodv
     print('Het getal',getal,'is zwart')
+    #auteur gijsbert keja
