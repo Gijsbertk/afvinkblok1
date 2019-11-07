0bde88991365d654e9487818c41ffeea4c8670d5
Gijsbertk <55399498+Gijsbertk@users.noreply.github.com>
2019-11-07 18:35:40 +0100

    Update 3.4.py

--- 3.4.py
+++ 3.4.py
@@ -1,8 +1,8 @@
-getal = int(input('Welk getal heeft u op de roulette? ')
-if getal == int(0):
-    print('het i groen')
+getal = int(input('Welk getal heeft u op de roulette? ') #vraagt om input
+if getal == int(0): #kijkt of de input gelijk is aan de statement
+    print('het i groen')#print groen als statement en input zelfde zijn
 
-elif getal == (1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36):
-    print('Het getal',getal,'is rood')
-elif: getal == (2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35):
-    print('Het getal'getal,'is zwart')
+elif getal == (1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36): #kijkt of de input gelijk is aan de statement
+    print('Het getal',getal,'is rood') #print rood als statement en input zelfde zijn
+elif: getal == (2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35): #kijkt of de input gelijk is aan de statement
+    print('Het getal'getal,'is zwart') #print zwrat als statement en input zelfde zijn
