0021c199f6ea8a7733afb5c84f7a97b7f67f5c4d
Gijsbertk <55399498+Gijsbertk@users.noreply.github.com>
2019-11-07 19:40:01 +0100

    Update 6.2.py

--- 6.2.py
+++ 6.2.py
@@ -1,10 +1,11 @@
 
-prog = ""
-maxx = 0
-bestand = intput(open('Bestandsnaam'))
-for n in bestand:
-    prog += bestand.readline()
-    maxx += 1
-    if maxx == 5:
-        break
-print (prog)
+prog = "" #variabele met spatie
+maxx = 0 #variabale met waarde 0
+bestand = intput(open('Bestandsnaam')) #opent bestand 
+for n in bestand: #itereert het aantal regels in bestand
+    prog += bestand.readline() #voegt regels toe aan bestand
+    maxx += 1 #een wordt toegevoegd bij elke iteratie
+    if maxx == 5: #stopt na 5 iteraties
+        break #breaked
+print (prog) #print de variabale
+#auteur Gijsbert Keja
