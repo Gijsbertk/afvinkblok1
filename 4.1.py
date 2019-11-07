f3cc3fe57c16303a7336d1f480ac4a817da76658
Gijsbertk <55399498+Gijsbertk@users.noreply.github.com>
2019-11-07 18:38:09 +0100

    Update 4.1.py

--- 4.1.py
+++ 4.1.py
@@ -1,6 +1,6 @@
-total = 0
+total = 0 #variabele om starks verder mee te rekeken
 
-for dag in range(5):
-    bugs = int(input('hoeveel insecten heeft u vandaag gevangen?'))
-    total = bugs + total
-print('u heeft deze week',total,'insecten gevangen')
+for dag in range(5): # itereert 5 keer
+    bugs = int(input('hoeveel insecten heeft u vandaag gevangen?')) # deze statement wordt 5 keer geïtereerd
+    total = bugs + total # de bugs worden per input bij elkaar opgeteld
+print('u heeft deze week',total,'insecten gevangen') # print het aantal bugs
