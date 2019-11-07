8b5d8bd739376209297704321055cbf5b80f4537
Gijsbertk <55399498+Gijsbertk@users.noreply.github.com>
2019-11-07 18:39:46 +0100

    Update 4.12.py

--- 4.12.py
+++ 4.12.py
@@ -1,6 +1,6 @@
-fac = 1
-getal = int(input('Waarvan wilt u de factoril weten?: '))
-for num in range(1, getal + 1):
-     fac = fac * num
-print (fac)
+fac = 1 #maakt variabele aan
+getal = int(input('Waarvan wilt u de factoril weten?: ')) #vraagt om input
+for num in range(1, getal + 1): #itereert het aantal keer wat de input is gegeven
+     fac = fac * num #de fac wordt uitgerekend dmv de aantal iteraties 
+print (fac) # print de fac
     
