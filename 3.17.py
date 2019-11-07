48bc1e5705e70fd2fb56cc2d97b42db300a5ada5
Gijsbertk <55399498+Gijsbertk@users.noreply.github.com>
2019-11-07 18:31:18 +0100

    Update 3.17.py

--- 3.17.py
+++ 3.17.py
@@ -1,14 +1,14 @@
-print('Probeer uw systeem opnieuw op te starten')
-antwoord = input('Heeft dit geholpen? ')
-if antwoord == 'nee':
-    print('start de router opnieuw op')
-    aww = input('heeft dit geholpen?')
-    if aww == 'nee':
-        print('Zorg ervoor dat u kabels in het systeem zitten')
-        awww = input('heeft dit geholpen? ')
-        if awww == 'nee':
-            print('verplaats uw router en zet deze op een andere locatie ')
-            aaww = input('heeft dit geholpen?')
-            if aaww == 'nee':
-                print('Koop een nieuwe router')
+print('Probeer uw systeem opnieuw op te starten') #print statement met een opdracht om uit te voeren
+antwoord = input('Heeft dit geholpen? ') #verwerkt antwoord bij if statement 
+if antwoord == 'nee': #voer het volgende blok uit als de input gelijk is aan de if statement oftewel als er nee wordt gegeven
+    print('start de router opnieuw op') #print statement met een opdracht om uit te voeren
+    aww = input('heeft dit geholpen?') #verwerkt antwoord bij if statement 
+    if aww == 'nee': #voer het volgende blok uit als de input gelijk is aan de if statement oftewel als er nee wordt gegeven
+        print('Zorg ervoor dat u kabels in het systeem zitten') #print statement met een opdracht om uit te voeren
+        awww = input('heeft dit geholpen? ') #verwerkt antwoord bij if statement 
+        if awww == 'nee': #voer het volgende blok uit als de input gelijk is aan de if statement oftewel als er nee wordt gegeven
+            print('verplaats uw router en zet deze op een andere locatie ') #print statement met een opdracht om uit te voeren
+            aaww = input('heeft dit geholpen?') #verwerkt antwoord bij if statement 
+            if aaww == 'nee': #voer het volgende blok uit als de input gelijk is aan de if statement oftewel als er nee wordt gegeven
+                print('Koop een nieuwe router') #print statement met een opdracht om uit te voeren
     
