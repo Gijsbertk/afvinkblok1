ef6fa01b7712407a5e09a955afffedf03ab6dd7f
Gijsbertk <55399498+Gijsbertk@users.noreply.github.com>
2019-11-07 19:37:20 +0100

    Update 5.21.py

--- 5.21.py
+++ 5.21.py
@@ -1,20 +1,20 @@
-import random
-print('Je doet steen papier schaar, voor steen kies je 1 voor papier 2 en schaar 3')
+import random #importeert library
+print('Je doet steen papier schaar, voor steen kies je 1 voor papier 2 en schaar 3') #print wat je moet doen
 
 
 def main():
-    game()
+    game() #roept functie aan
 
 
 
-def game():
-    speler = int(input('Kies een nummer voor steen papier schaar: '))
-    comp = random.randint(1, 3)
-    print(comp)
+def game(): #functie
+    speler = int(input('Kies een nummer voor steen papier schaar: ')) #vraagt om functie
+    comp = random.randint(1, 3) # random getal wordt toegekend aan variabele
+    print(comp) #print varaiabele
 
-    while speler == comp:
-        speler = int(input('Kies een nummer voor steen papier schaar: '))
-        comp = random.randint(1, 3)
+    while speler == comp: # als variabele gelijk is aan input
+        speler = int(input('Kies een nummer voor steen papier schaar: ')) #opnieuw
+        comp = random.randint(1, 3) # opnieuw
         
     
     
@@ -26,3 +26,4 @@ def game():
 
 
 main()
+#auteur gijsbert ekja
