3cdd3c57acc75df22b7ff58fe436196af3a07c26
Gijsbertk <55399498+Gijsbertk@users.noreply.github.com>
2019-11-07 18:51:24 +0100

    Update 5.15.py

--- 5.15.py
+++ 5.15.py
@@ -1,39 +1,39 @@
 def main():
-    test1, test2, test3, test4, test5 = inputt()
-    gemiddel = gemiddelde(test1, test2, test3, test4, test5)
-    score(gemiddel)
+    test1, test2, test3, test4, test5 = inputt() #roept functie aan en returned variabele en verwerkt de return
+    gemiddel = gemiddelde(test1, test2, test3, test4, test5) #roept functie aan en returned variabele en verwerkt de return
+    score(gemiddel) #roept functie aan en verwerkt de return
 
 
-def score(gemiddel):
-    if gemiddel >= 90 and gemiddel <= 100:
-        resultaat = 'A'
-        print('Met een gemiddelde van',gemiddel,'heb je een',resultaat,'behaald')
-    if gemiddel >= 80 and gemiddel <= 89:
-        resultaat = 'B'
-        print('Met een gemiddelde van',gemiddel,'heb je een',resultaat,'behaald')
-    if gemiddel >= 70 and gemiddel <= 79:
-        resultaat = 'C'
-        print('Met een gemiddelde van',gemiddel,'heb je een',resultaat,'behaald')
-    if gemiddel >= 60 and gemiddel <= 69:
-        resultaat = 'D'
-        print('Met een gemiddelde van',gemiddel,'heb je een',resultaat,'behaald')
-    if gemiddel <= 59:
-        resultaat = 'F'
-        print('Met een gemiddelde van',gemiddel,'heb je een',resultaat,'behaald')       
+def score(gemiddel):  #variabelen worden verwerkt in functie 
+    if gemiddel >= 90 and gemiddel <= 100: #vergelijkt gemiddelde met een if statement
+        resultaat = 'A' #als if statement true is dan wordt deze variabelen deze waarde gegeven 
+        print('Met een gemiddelde van',gemiddel,'heb je een',resultaat,'behaald') #print de variabele en score
+    if gemiddel >= 80 and gemiddel <= 89: #vergelijkt gemiddelde met een if statement
+        resultaat = 'B' #als if statement true is dan wordt deze variabelen deze waarde gegeven
+        print('Met een gemiddelde van',gemiddel,'heb je een',resultaat,'behaald') #print de variabele en score
+    if gemiddel >= 70 and gemiddel <= 79:  #vergelijkt gemiddelde met een if statement
+        resultaat = 'C' #als if statement true is dan wordt deze variabelen deze waarde gegeven 
+        print('Met een gemiddelde van',gemiddel,'heb je een',resultaat,'behaald') #print de variabele en score
+    if gemiddel >= 60 and gemiddel <= 69:  #vergelijkt gemiddelde met een if statement
+        resultaat = 'D' #als if statement true is dan wordt deze variabelen deze waarde gegeven 
+        print('Met een gemiddelde van',gemiddel,'heb je een',resultaat,'behaald') #print de variabele en score
+    if gemiddel <= 59: #als de gemiddelde lager is dan 59 wordt is het altijd true
+        resultaat = 'F' #als if statement true is dan wordt deze variabelen deze waarde gegeven 
+        print('Met een gemiddelde van',gemiddel,'heb je een',resultaat,'behaald') #print de variabele en score      
 
 
-def gemiddelde(test1, test2, test3, test4, test5):
-    gemiddel = (test1 + test2 + test3 + test4 + test5) / 5
-    return gemiddel
+def gemiddelde(test1, test2, test3, test4, test5): #variabelen worden verwerkt in functie 
+    gemiddel = (test1 + test2 + test3 + test4 + test5) / 5 #berekent gemiddlede van de scores
+    return gemiddel #retured de variabele
 
 
 def inputt():
-    test1 = float(input('Wat was je score?: '))
-    test2 = float(input('Wat was je score?: '))
-    test3 = float(input('Wat was je score?: '))
-    test4 = float(input('Wat was je score?: '))
-    test5 = float(input('Wat was je score?: '))
-    return test1, test2, test3, test4, test5 
+    test1 = float(input('Wat was je score?: ')) # vraagt voor input score
+    test2 = float(input('Wat was je score?: ')) # vraagt voor input score
+    test3 = float(input('Wat was je score?: ')) # vraagt voor input score
+    test4 = float(input('Wat was je score?: ')) # vraagt voor input score
+    test5 = float(input('Wat was je score?: ')) # vraagt voor input score
+    return test1, test2, test3, test4, test5 # returned de variabelen
 
 
 
