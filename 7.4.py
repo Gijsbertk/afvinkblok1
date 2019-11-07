c1b05822180103e8eaf8c448949b2dfb44c7190d
Gijsbertk <55399498+Gijsbertk@users.noreply.github.com>
2019-11-07 19:59:48 +0100

    Update 7.4.py

--- 7.4.py
+++ 7.4.py
@@ -1,20 +1,21 @@
-nummers = []
-for vragen in range(20):
-     x = input ("voer 20 nummers in")
-     nummers.append(B)
-gem = 0
-get = 0
-laagste = nummers[0]
-hoogste = nummers[0]
-for y in nummers:
-    gemiddeld += int (y)
-    get += int (y)
-    if int (laagste) > int (y):
-        laagste = y
-    elif int (hoogste) < int (y):
-        hoogste = y
-gemiddeld = gemiddeld / len(nummers)
-print (gemiddeld)
-print (get)
-rint ("laagste " + laagste)  
-print ("hoogste " + laagste)
+nummers = [] #lege list 
+for vragen in range(20): # 20 iteraties
+     B = input ("voer 20 nummers in") # itereert
+     nummers.append(B) # schrijft elke eer bij 
+gem = 0  #variabele met waarde 0
+get = 0 #variabele met waarde 0
+laagste = nummers[0] #variabele wordt nummer in list aangeroepen
+hoogste = nummers[0] #variabele wordt nummer in list aangeroepen
+for y in nummers: #itereert de aantal nummers
+    gemiddeld += int (y) #voegt na de aantal ieteraties waarde toe aan var
+    get += int (y) #voegt na de aantal ieteraties waarde toe aan var
+    if int (laagste) > int (y): #if staement met gorter dan variabele
+        laagste = y #laagste krijgt een nieuwe waarde
+    elif int (hoogste) < int (y): #if staement met kleiner dan variabele
+        hoogste = y #hoogste krijgt een nieuwe waarde
+gemiddeld = gemiddeld / len(nummers) #berkent gemiddlede
+print (gemiddeld) #print var
+print (get) #print var
+print ("laagste " + laagste) #print var 
+print ("hoogste " + laagste) #print var
+#auteur gijsbert keja
