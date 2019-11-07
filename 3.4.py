572da0ea0b35c387ff32ce8d859ff522be21abc3
Gijsbertk <55399498+Gijsbertk@users.noreply.github.com>
2019-11-07 20:16:06 +0100

    Update 3.4.py

--- 3.4.py
+++ 3.4.py
@@ -6,3 +6,4 @@ elif getal == (1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36): #kijkt of de i
     print('Het getal',getal,'is rood') #print rood als statement en input zelfde zijn
 elif: getal == (2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35): #kijkt of de input gelijk is aan de statement
     print('Het getal'getal,'is zwart') #print zwrat als statement en input zelfde zijn
+#auteur gijsbert keja
