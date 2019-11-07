b625bd82be043d356516c86cb4214fb03a72b3b1
Gijsbertk <55399498+Gijsbertk@users.noreply.github.com>
2019-11-07 18:43:30 +0100

    Update 4.3.py

--- 4.3.py
+++ 4.3.py
@@ -1,22 +1,22 @@
-rondje = int(input('Hoeveel rondjes heeft u gelopen?: '))
-total = 0.0
-langzaamste = 0
-snelste = 999
+rondje = int(input('Hoeveel rondjes heeft u gelopen?: ')) #vraagt om het aantal rodnjes
+total = 0.0 #variabele voor later
+langzaamste = 0 #variabele voor later
+snelste = 999 #variabele voor later
 
-for tijd in range(rondje):
-    rondetijd = float(input('Wat zijn u rondetijden?: '))
-    total = total + rondetijd
-    if rondetijd > langzaamste:
-        langzaamste = rondetijd
-    if rondetijd < snelste:
-        snelste = rondetijd
+for tijd in range(rondje): #itereert aantal keer het rondje
+    rondetijd = float(input('Wat zijn u rondetijden?: ')) #vraagt om de rondetijd aantal keer de iteraties 
+    total = total + rondetijd #berekent de totale rondetijd
+    if rondetijd > langzaamste: #berekent de langzaamst
+        langzaamste = rondetijd #geeft de snelste een variabal
+    if rondetijd < snelste: #geeft de snelste een variabale
+        snelste = rondetijd #berekent de langzaamst
     
 
 
     
-gemiddelde = total / rondje    
-print(gemiddelde)
-print(snelste)
-print(langzaamste)
+gemiddelde = total / rondje   #berekent het gemiddelde 
+print(gemiddelde) #print het gemiddelde
+print(snelste) #print het snelste
+print(langzaamste) #print het langzaamste
                     
                    
